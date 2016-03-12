fcb01fdf4df9ab4a86d52fb41877b903979f8292
Raoul Cristian <Raoul Cristian>
2016-03-12 22:22:55 +0200

    products

--- app/scripts/controllers/products.js
+++ app/scripts/controllers/products.js
@@ -8,6 +8,90 @@
  * Controller of the fermaApp
  */
 angular.module('fermaApp')
-  .controller('ProductsCtrl', function ($scope, dataService) {
+  .controller('ProductsCtrl', function ($scope, dataService, $filter) {
+
+    $scope.categories = [
+      {
+        id:1,
+        name: 'branza'
+      },
+      {
+        id:2,
+        name: 'lapte'
+      },
+      {
+        id:3,
+        name: 'animale'
+      },
+      {
+        id:4,
+        name: 'carne'
+      }
+    ];
+
+    $scope.productItems = [
+      {
+        image: 'http://lorempixel.com/700/400/',
+        name:'First name',
+        category:'branza'
+      },
+      {
+        image: 'http://lorempixel.com/300/400/',
+        name:'First name 2',
+        category:'branza'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'branza'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'lapte'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'lapte'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'animale'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'animale'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'animale'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'carne'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'carne'
+      },
+      {
+        image: 'http://lorempixel.com/600/800/',
+        name:'First name 2',
+        category:'carne'
+      }
+    ];
+
+
+    $scope.setFilter = function(category){
+
+    }
+
+
 
   });
